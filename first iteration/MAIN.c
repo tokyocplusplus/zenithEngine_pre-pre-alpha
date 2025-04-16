88d751114922db0f6760475142476ee71807064a
- [zenith] - <tokyocplusplus@gmail.com>
2025-04-16 19:59:05 +1000

    Create MAIN.c

---
+++ first iteration/MAIN.c
@@ -0,0 +1,11 @@
+#include<stdlib.h>
+#include<stdio.h>
+//use offsetof() and sizeof() as much as possible, it simplifies your code
+
+int main() {
+    //int x;
+    //printf("the size of a single integer is... %zu!!\n", sizeof(x));
+    //printf("optimization notes...\nokay so for some stupid reason, rendering more small triangles can run faster than a few massive triangles\nmore pixels=more memory usage and bottleneck\nmore triangles=more memory usage and bottleneck\nBUT if its small, theres less memory usage because of less pixels\nwtf is going on\n");
+    printf("C CODE PRINT:3!!\n");
+    return 0;
+}
